434e9e015f89ff6a7c31401db3f4abb80ba46d64
SY Lee <Three_God@naver.com>
2022-12-23 18:19:55 +0900

    Initial Commit

---
+++ OpggClone/OpggCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpggCloneApp.swift
+//  OpggClone
+//
+//  Created by Sy Lee on 2022/12/23.
+//
+
+import SwiftUI
+
+@main
+struct OpggCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
