0dc37bb1f47029475711d83bf7297570b921c741
Maru Kim <kiapha1108@gmail.com>
2023-01-05 18:49:34 +0900

    second terminal commit

---
+++ OpggClone/MainVM.swift
@@ -0,0 +1,8 @@
+//
+//  MainVM.swift
+//  OpggClone
+//
+//  Created by Sy Lee on 2023/01/04.
+//
+
+import Foundation
---
+++ OpggClone/service.swift
@@ -0,0 +1,8 @@
+//
+//  service.swift
+//  OpggClone
+//
+//  Created by Sy Lee on 2023/01/05.
+//
+
+import Foundation
