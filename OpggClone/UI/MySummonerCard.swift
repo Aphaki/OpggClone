f7347de8754b4ffe132d4096aafba40081876849
Maru Kim <kiapha1108@gmail.com>
2023-01-06 23:57:47 +0900

    first terminal commit

---
+++ OpggClone/UI/MySummonerCard.swift
@@ -0,0 +1,20 @@
+//
+//  MySummonerCard.swift
+//  OpggClone
+//
+//  Created by Sy Lee on 2023/01/06.
+//
+
+import SwiftUI
+
+struct MySummonerCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MySummonerCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MySummonerCard()
+    }
+}
